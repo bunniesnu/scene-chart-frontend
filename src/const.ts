495bd97a224f4e3fb601527a7da90edf19198f77
Bunniesnu <bunniesnu@gmail.com>
2026-08-26 02:27:13 +0900

    Add const.ts

---
+++ src/const.ts
@@ -0,0 +1,3 @@
+import type { ChartType } from "./types/chart";
+
+export const defaultChartType: ChartType = "top100"
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { createFileRoute } from '@tanstack/react-router'
 import ChartTable from "@/components/chart/table"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { chartTypeLabels, chartTypes } from "@/types/chart"
+import { defaultChartType } from '@/const';
 
 export const Route = createFileRoute('/')({
   component: Index,
@@ -9,7 +10,7 @@ export const Route = createFileRoute('/')({
 
 function Index() {
   return <>
-    <Tabs className="w-full max-w-xl flex flex-col items-center justify-center gap-4 p-4" defaultValue="top100">
+    <Tabs className="w-full max-w-xl flex flex-col items-center justify-center gap-4 p-4" defaultValue={defaultChartType}>
       <TabsList>
         {chartTypes.map((type) => (
           <TabsTrigger key={type} value={type}>
