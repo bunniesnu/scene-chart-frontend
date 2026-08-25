76f830a0e69b7bca7c143780e1a5f2eb400a0a91
Bunniesnu <bunniesnu@gmail.com>
2026-08-26 00:24:31 +0900

    Add placeholders for chart data isLoading, error and no data

--- src/components/chart/table.tsx
+++ src/components/chart/table.tsx
@@ -40,34 +40,40 @@ function ChartTable(chartTableProps: ChartTableProps) {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {data?.entries.map((item, index) => {
-            let rankChangeBadge = <Badge className="bg-gray-200 text-gray-400 text-sm text-center min-w-11">
-              -
-            </Badge>
-            if (item.snapshot.rank_type === "UP") {
-              rankChangeBadge = <Badge className="bg-red-200 text-red-400 text-sm text-center min-w-11">
-                <Triangle fill="currentColor" />
-                {item.snapshot.rank_gap}
+          {isLoading ? <TableRow>
+            <TableCell colSpan={3} className="text-center text-gray-400 h-20">Loading...</TableCell>
+          </TableRow> : (error ? <TableRow>
+              <TableCell colSpan={3} className="text-center text-gray-400 h-20">Error</TableCell>
+            </TableRow> : (data ? data.entries.map((item, index) => {
+              let rankChangeBadge = <Badge className="bg-gray-200 text-gray-400 text-sm text-center min-w-11">
+                -
               </Badge>
-            } else if (item.snapshot.rank_type === "DOWN") {
-              rankChangeBadge = <Badge className="bg-green-200 text-green-400 text-sm text-center min-w-11">
-                <Triangle fill="currentColor" className="rotate-180" />
-                {item.snapshot.rank_gap}
-              </Badge>
-            }
-            return (
-              <TableRow key={item.song.song_id}>
-                <TableCell className="text-center text-lg">{item.snapshot.current_rank}</TableCell>
-                <TableCell>
-                  <img src={`//wsrv.nl?url=${item.song.album_cover_url?.split("?")[0]}&w=100&h=100&output=webp&il`} className="inline-block mr-2 w-10 h-10 rounded-md" />
-                  {item.song.title}
-                </TableCell>
-                <TableCell className="text-center">
-                  {rankChangeBadge}
-                </TableCell>
-              </TableRow>
-            )
-          })}
+              if (item.snapshot.rank_type === "UP") {
+                rankChangeBadge = <Badge className="bg-red-200 text-red-400 text-sm text-center min-w-11">
+                  <Triangle fill="currentColor" />
+                  {item.snapshot.rank_gap}
+                </Badge>
+              } else if (item.snapshot.rank_type === "DOWN") {
+                rankChangeBadge = <Badge className="bg-green-200 text-green-400 text-sm text-center min-w-11">
+                  <Triangle fill="currentColor" className="rotate-180" />
+                  {item.snapshot.rank_gap}
+                </Badge>
+              }
+              return (
+                <TableRow key={item.song.song_id}>
+                  <TableCell className="text-center text-lg">{item.snapshot.current_rank}</TableCell>
+                  <TableCell>
+                    <img src={`//wsrv.nl?url=${item.song.album_cover_url?.split("?")[0]}&w=100&h=100&output=webp&il`} className="inline-block mr-2 w-10 h-10 rounded-md" />
+                    {item.song.title}
+                  </TableCell>
+                  <TableCell className="text-center">
+                    {rankChangeBadge}
+                  </TableCell>
+                </TableRow>
+              )
+            }) : <TableRow>
+              <TableCell colSpan={3} className="text-center text-gray-400 h-20">No data</TableCell>
+            </TableRow>))}
         </TableBody>
       </Table>
     </div>
