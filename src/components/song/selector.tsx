6b920cbab6031ebf7bdc7fcd18a8a49ca5c40e8b
Bunniesnu <bunniesnu@gmail.com>
2026-09-03 02:16:50 +0900

    Format duplicate components inside SongSelector

--- src/components/song/selector.tsx
+++ src/components/song/selector.tsx
@@ -38,72 +38,15 @@ export function SongSelectorMultiple({
   value,
   onValueChange,
 }: SongSelectorPropsMultiple) {
-  const [open, setOpen] = useState(false)
-
   const selectedSongs = songs.filter((song) => value.includes(song.song_id))
-
-  return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger>
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={open}
-          className="w-full justify-between"
-        >
-          {selectedSongs.length === 0
-            ? "Select songs..."
-            : `${selectedSongs.length} songs selected`}
-          <ChevronsUpDown className="ml-2 size-4 shrink-0 opacity-50" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent
-        className="w-(--radix-popover-trigger-width) p-0"
-        align="center"
-      >
-        <Command>
-          <CommandInput placeholder="Search songs..." />
-          <CommandList>
-            <CommandEmpty>No songs found.</CommandEmpty>
-            <CommandGroup>
-              {songs.map((song) => (
-                <CommandItem
-                  key={song.song_id}
-                  value={`${song.title}`}
-                  onSelect={() => {
-                    onValueChange(song.song_id)
-                  }}
-                >
-                  <div className="flex min-w-full items-center gap-3">
-                    {song.album_cover_url && (
-                      <img
-                        src={song.album_cover_url}
-                        alt=""
-                        className="size-10 shrink-0 rounded"
-                      />
-                    )}
-                    <div className="min-w-0">
-                      <p className="truncate font-medium">
-                        {song.title}
-                      </p>
-                    </div>
-                    <Check
-                      className={cn(
-                        "ml-auto size-4",
-                        value.includes(song.song_id)
-                          ? "opacity-100"
-                          : "opacity-0"
-                      )}
-                    />
-                  </div>
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </CommandList>
-        </Command>
-      </PopoverContent>
-    </Popover>
-  )
+  const message = selectedSongs.length === 0 ? "Select songs..." : `${selectedSongs.length} songs selected`
+  return <SongSelector
+    songs={songs}
+    value={value}
+    onValueChange={onValueChange}
+    message={message}
+    closeOnSelect={false}
+  />
 }
 
 export function SongSelectorSingle({
@@ -111,10 +54,33 @@ export function SongSelectorSingle({
   value,
   onValueChange,
 }: SongSelectorPropsSingle) {
-  const [open, setOpen] = useState(false)
-
   const selectedSong = songs.find((song) => song.song_id === value)
+  const message = selectedSong ? selectedSong.title : "Select a song..."
+  return <SongSelector
+    songs={songs}
+    value={value}
+    onValueChange={onValueChange}
+    message={message}
+    closeOnSelect={true}
+  />
+}
 
+interface SongSelectorProps {
+  songs: Song[]
+  value: string | string[]
+  onValueChange: (songId: string) => void
+  message: string
+  closeOnSelect: boolean
+}
+
+function SongSelector({
+  songs,
+  value,
+  onValueChange,
+  message,
+  closeOnSelect,
+}: SongSelectorProps) {
+  const [open, setOpen] = useState(false)
   return (
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger>
@@ -124,9 +90,7 @@ export function SongSelectorSingle({
           aria-expanded={open}
           className="w-full justify-between"
         >
-          {selectedSong === undefined
-            ? "Select song..."
-            : selectedSong.title}
+          {message}
           <ChevronsUpDown className="ml-2 size-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
@@ -144,7 +108,7 @@ export function SongSelectorSingle({
                   key={song.song_id}
                   value={`${song.title}`}
                   onSelect={() => {
-                    setOpen(false)
+                    closeOnSelect && setOpen(false)
                     onValueChange(song.song_id)
                   }}
                 >
@@ -164,7 +128,7 @@ export function SongSelectorSingle({
                     <Check
                       className={cn(
                         "ml-auto size-4",
-                        value === song.song_id
+                        (Array.isArray(value) ? value.includes(song.song_id) : value === song.song_id)
                           ? "opacity-100"
                           : "opacity-0"
                       )}
