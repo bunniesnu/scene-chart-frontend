ca060d93617dd7f10e4ba45b8d7a02d8abca8203
Bunniesnu <bunniesnu@gmail.com>
2026-09-03 03:56:07 +0900

    Update api.d.ts

--- src/api.d.ts
+++ src/api.d.ts
@@ -55,23 +55,6 @@ export interface paths {
         patch?: never;
         trace?: never;
     };
-    "/debug/melon-db": {
-        parameters: {
-            query?: never;
-            header?: never;
-            path?: never;
-            cookie?: never;
-        };
-        /** Dump Melon Database */
-        get: operations["dump_melon_database_debug_melon_db_get"];
-        put?: never;
-        post?: never;
-        delete?: never;
-        options?: never;
-        head?: never;
-        patch?: never;
-        trace?: never;
-    };
 }
 export type webhooks = Record<string, never>;
 export interface components {
@@ -253,8 +236,6 @@ export interface operations {
         parameters: {
             query: {
                 songId: string;
-                from?: string | null;
-                to?: string | null;
             };
             header?: never;
             path: {
@@ -280,26 +261,6 @@ export interface operations {
                 };
                 content: {
                     "application/json": components["schemas"]["HTTPValidationError"];
-                };
-            };
-        };
-    };
-    dump_melon_database_debug_melon_db_get: {
-        parameters: {
-            query?: never;
-            header?: never;
-            path?: never;
-            cookie?: never;
-        };
-        requestBody?: never;
-        responses: {
-            /** @description Successful Response */
-            200: {
-                headers: {
-                    [name: string]: unknown;
-                };
-                content: {
-                    "application/json": unknown;
                 };
             };
         };
