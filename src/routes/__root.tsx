c3f10acff0233bdb7590b0bfe058ac0579bae139
Bunniesnu <bunniesnu@gmail.com>
2026-09-03 00:05:40 +0900

    Add navigator

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,45 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { Link, Outlet, createRootRoute } from '@tanstack/react-router'
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+import { Separator } from '@/components/ui/separator';
 
 export const Route = createRootRoute({
   component: RootComponent,
 })
 
 function RootComponent() {
-  return <div className="flex flex-col items-center mx-auto min-h-screen w-screen max-w-3xl">
+  return <div className="flex flex-col mx-auto min-h-screen w-screen max-w-3xl pt-4">
+    <NavigationMenu align="start" className="flex-0 pb-4">
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link
+            to="/"
+            className={navigationMenuTriggerStyle()}
+            activeProps={{
+              className: "bg-accent text-accent-foreground",
+            }}
+          >
+            Home
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link
+            to="/history"
+            className={navigationMenuTriggerStyle()}
+            activeProps={{
+              className: "bg-accent text-accent-foreground",
+            }}
+          >
+            History
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+    <Separator />
     <Outlet />
   </div>
 }
