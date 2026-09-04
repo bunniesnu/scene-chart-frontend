f2f679b41e84f936f7cb2fef4529a7b43525154a
Bunniesnu <bunniesnu@gmail.com>
2026-09-04 15:58:29 +0900

    Add chartType query & optimize tabs

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,24 @@
 import { createFileRoute } from '@tanstack/react-router'
 import ChartTable from "@/components/chart/table"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { chartTypeLabels, chartTypes } from "@/types/chart"
+import { chartTypeLabels, chartTypes, type ChartType } from "@/types/chart"
 import { defaultChartType } from '@/const';
 
 export const Route = createFileRoute('/')({
+  validateSearch: (search) => {
+    return {
+      chartType: chartTypes.includes(search.chartType as ChartType)
+        ? (search.chartType as ChartType)
+        : defaultChartType,
+    }
+  },
   component: Index,
 })
 
 function Index() {
-return <Tabs className="w-full flex flex-col items-center justify-center gap-4 p-4" defaultValue={defaultChartType}>
+  const { chartType } = Route.useSearch()
+  const navigate = Route.useNavigate()
+  return <Tabs className="w-full flex flex-col items-center justify-center gap-4 p-4" value={chartType} onValueChange={(value) => navigate({ search: { chartType: value } })}>
     <TabsList>
       {chartTypes.map((type) => (
         <TabsTrigger key={type} value={type}>
@@ -17,10 +26,8 @@ return <Tabs className="w-full flex flex-col items-center justify-center gap-4 p
         </TabsTrigger>
       ))}
     </TabsList>
-    {chartTypes.map((type) => (
-      <TabsContent key={type} value={type} className="w-full">
-        <ChartTable chartType={type} />
-      </TabsContent>
-    ))}
+    <TabsContent value={chartType} className="w-full">
+      <ChartTable chartType={chartType} />
+    </TabsContent>
   </Tabs>
 }
